361ec1f0bc12ceb6b538ba32cd429a5820e76d0e
Peter Forbes <66070260+pjf1822@users.noreply.github.com>
2022-03-02 15:36:15 -0500

    first push
    first push

--- App.js
+++ App.js
@@ -1,11 +1,90 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Image,
+  Dimensions,
+} from "react-native";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { dataStuff } from "./utils/apiFetch";
 
 export default function App() {
+  const [data, setData] = useState([]);
+  const [rapper, setRapper] = useState("drake");
+  const [switchFlip, setSwitchFlip] = useState(true);
+
+  var options = {
+    method: "GET",
+    url: "https://genius.p.rapidapi.com/search",
+    params: { q: `${rapper}` },
+    headers: {
+      "x-rapidapi-host": "genius.p.rapidapi.com",
+      "x-rapidapi-key": "c899e26ce2msh026ef0e795b08b6p1c91dajsn63fcd078f421",
+    },
+  };
+
+  useEffect(() => {
+    setData(dataStuff);
+    // axios
+    //   .request(options)
+    //   .then(function (response) {
+    //     setData(response.data.response.hits);
+    //   })
+    //   .catch(function (error) {
+    //     console.error(error);
+    //   });
+  }, [switchFlip]);
+
+  // rerun api pull
+
+  const pullRapper = () => {
+    setSwitchFlip(!switchFlip);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View
+        style={{
+          height: 200,
+          width: Dimensions.get("window").width,
+          flex: 1,
+          flexDirection: "row",
+        }}
+      >
+        {data.map((d) => {
+          const imagething = d.result.song_art_image_thumbnail_url;
+          let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+          return (
+            <View
+              style={{
+                flex: 1,
+                justifyContent: "center",
+                alignItems: "center",
+                backgroundColor: `#${randomColor}`,
+              }}
+              key={d.id}
+            >
+              <Text>{d.result.full_title}</Text>
+              <Image
+                source={{
+                  uri: imagething,
+                }}
+                style={styles.imageStyle}
+              />
+            </View>
+          );
+        })}
+      </View>
+      <View>
+        <TextInput
+          onChangeText={(value) => setRapper(value)}
+          placeholder="Search"
+          style={{ backgroundColor: "#fff", paddingHorizontal: 20 }}
+          onSubmitEditing={(value) => pullRapper(value.nativeEvent.text)}
+        />
+      </View>
     </View>
   );
 }
@@ -13,8 +92,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  imageStyle: { height: 50, width: 50 },
 });
---
+++ utils/apiFetch.js
@@ -0,0 +1,448 @@
+export const dataStuff = [
+  {
+    highlights: [],
+    index: "song",
+    type: "song",
+    result: {
+      annotation_count: 20,
+      api_path: "/songs/3315890",
+      artist_names: "Drake",
+      full_title: "God's Plan by Drake",
+      header_image_thumbnail_url:
+        "https://images.genius.com/5a2f765ab113cbecf2486353f59a348d.300x300x1.jpg",
+      header_image_url:
+        "https://images.genius.com/5a2f765ab113cbecf2486353f59a348d.1000x1000x1.jpg",
+      id: 3315890,
+      lyrics_owner_id: 1507505,
+      lyrics_state: "complete",
+      path: "/Drake-gods-plan-lyrics",
+      pyongs_count: 922,
+      song_art_image_thumbnail_url:
+        "https://images.genius.com/ae0cd04ff9417b23861f674772ded07f.300x300x1.jpg",
+      song_art_image_url:
+        "https://images.genius.com/ae0cd04ff9417b23861f674772ded07f.1000x1000x1.jpg",
+      stats: {
+        unreviewed_annotations: 0,
+        concurrents: 3,
+        hot: false,
+        pageviews: 9089274,
+      },
+      title: "God’s Plan",
+      title_with_featured: "God's Plan",
+      url: "https://genius.com/Drake-gods-plan-lyrics",
+      primary_artist: {
+        api_path: "/artists/130",
+        header_image_url:
+          "https://images.genius.com/19df8d21c9ac35d1503d82f9fdbf367f.1000x563x1.jpg",
+        id: 130,
+        image_url:
+          "https://images.genius.com/99479447145e32877d46d53c7e5bf259.961x961x1.png",
+        is_meme_verified: false,
+        is_verified: false,
+        name: "Drake",
+        url: "https://genius.com/artists/Drake",
+      },
+    },
+  },
+  {
+    highlights: [],
+    index: "song",
+    type: "song",
+    result: {
+      annotation_count: 14,
+      api_path: "/songs/3807759",
+      artist_names: "Drake",
+      full_title: "In My Feelings by Drake",
+      header_image_thumbnail_url:
+        "https://images.genius.com/5c677b7bfc2f696abf2e2d928301bb44.300x300x1.jpg",
+      header_image_url:
+        "https://images.genius.com/5c677b7bfc2f696abf2e2d928301bb44.1000x1000x1.jpg",
+      id: 3807759,
+      lyrics_owner_id: 4424691,
+      lyrics_state: "complete",
+      path: "/Drake-in-my-feelings-lyrics",
+      pyongs_count: 210,
+      song_art_image_thumbnail_url:
+        "https://images.genius.com/5c677b7bfc2f696abf2e2d928301bb44.300x300x1.jpg",
+      song_art_image_url:
+        "https://images.genius.com/5c677b7bfc2f696abf2e2d928301bb44.1000x1000x1.jpg",
+      stats: {
+        unreviewed_annotations: 0,
+        concurrents: 2,
+        hot: false,
+        pageviews: 7390856,
+      },
+      title: "In My Feelings",
+      title_with_featured: "In My Feelings",
+      url: "https://genius.com/Drake-in-my-feelings-lyrics",
+      primary_artist: {
+        api_path: "/artists/130",
+        header_image_url:
+          "https://images.genius.com/19df8d21c9ac35d1503d82f9fdbf367f.1000x563x1.jpg",
+        id: 130,
+        image_url:
+          "https://images.genius.com/99479447145e32877d46d53c7e5bf259.961x961x1.png",
+        is_meme_verified: false,
+        is_verified: false,
+        name: "Drake",
+        url: "https://genius.com/artists/Drake",
+      },
+    },
+  },
+  {
+    highlights: [],
+    index: "song",
+    type: "song",
+    result: {
+      annotation_count: 10,
+      api_path: "/songs/2263723",
+      artist_names: "Drake",
+      full_title: "Hotline Bling by Drake",
+      header_image_thumbnail_url:
+        "https://images.genius.com/615f00c44861bf07488b9c262183fda0.300x300x1.png",
+      header_image_url:
+        "https://images.genius.com/615f00c44861bf07488b9c262183fda0.1000x1000x1.png",
+      id: 2263723,
+      lyrics_owner_id: 744505,
+      lyrics_state: "complete",
+      path: "/Drake-hotline-bling-lyrics",
+      pyongs_count: 639,
+      song_art_image_thumbnail_url:
+        "https://images.genius.com/f3be0158d3a067a81b075686a3a2e63d.300x300x1.png",
+      song_art_image_url:
+        "https://images.genius.com/f3be0158d3a067a81b075686a3a2e63d.1000x1000x1.png",
+      stats: {
+        unreviewed_annotations: 1,
+        concurrents: 2,
+        hot: false,
+        pageviews: 6868063,
+      },
+      title: "Hotline Bling",
+      title_with_featured: "Hotline Bling",
+      url: "https://genius.com/Drake-hotline-bling-lyrics",
+      primary_artist: {
+        api_path: "/artists/130",
+        header_image_url:
+          "https://images.genius.com/19df8d21c9ac35d1503d82f9fdbf367f.1000x563x1.jpg",
+        id: 130,
+        image_url:
+          "https://images.genius.com/99479447145e32877d46d53c7e5bf259.961x961x1.png",
+        is_meme_verified: false,
+        is_verified: false,
+        name: "Drake",
+        url: "https://genius.com/artists/Drake",
+      },
+    },
+  },
+  {
+    highlights: [],
+    index: "song",
+    type: "song",
+    result: {
+      annotation_count: 11,
+      api_path: "/songs/2450584",
+      artist_names: "Drake (Ft. Kyla & Wizkid)",
+      full_title: "One Dance by Drake (Ft. Kyla & Wizkid)",
+      header_image_thumbnail_url:
+        "https://images.rapgenius.com/c16287e16fcdaf2b2d9bec0b5634641f.300x300x1.jpg",
+      header_image_url:
+        "https://images.rapgenius.com/c16287e16fcdaf2b2d9bec0b5634641f.600x600x1.jpg",
+      id: 2450584,
+      lyrics_owner_id: 104344,
+      lyrics_state: "complete",
+      path: "/Drake-one-dance-lyrics",
+      pyongs_count: 232,
+      song_art_image_thumbnail_url:
+        "https://images.genius.com/b94353bfd9c57fd0cf88677ffb777193.300x300x1.png",
+      song_art_image_url:
+        "https://images.genius.com/b94353bfd9c57fd0cf88677ffb777193.1000x1000x1.png",
+      stats: {
+        unreviewed_annotations: 0,
+        hot: false,
+        pageviews: 5985556,
+      },
+      title: "One Dance",
+      title_with_featured: "One Dance (Ft. Kyla & Wizkid)",
+      url: "https://genius.com/Drake-one-dance-lyrics",
+      primary_artist: {
+        api_path: "/artists/130",
+        header_image_url:
+          "https://images.genius.com/19df8d21c9ac35d1503d82f9fdbf367f.1000x563x1.jpg",
+        id: 130,
+        image_url:
+          "https://images.genius.com/99479447145e32877d46d53c7e5bf259.961x961x1.png",
+        is_meme_verified: false,
+        is_verified: false,
+        name: "Drake",
+        url: "https://genius.com/artists/Drake",
+      },
+    },
+  },
+  {
+    highlights: [],
+    index: "song",
+    type: "song",
+    result: {
+      annotation_count: 8,
+      api_path: "/songs/200546",
+      artist_names: "Drake (Ft. Majid Jordan)",
+      full_title: "Hold On, We're Going Home by Drake (Ft. Majid Jordan)",
+      header_image_thumbnail_url:
+        "https://images.genius.com/92ae1cfe791720da44d95b13bc9f67a2.300x300x1.jpg",
+      header_image_url:
+        "https://images.genius.com/92ae1cfe791720da44d95b13bc9f67a2.1000x1000x1.jpg",
+      id: 200546,
+      lyrics_owner_id: 149650,
+      lyrics_state: "complete",
+      path: "/Drake-hold-on-were-going-home-lyrics",
+      pyongs_count: 505,
+      song_art_image_thumbnail_url:
+        "https://images.rapgenius.com/f545371242592e32983526102bd7b01d.300x300x1.jpg",
+      song_art_image_url:
+        "https://images.rapgenius.com/f545371242592e32983526102bd7b01d.600x600x1.jpg",
+      stats: {
+        unreviewed_annotations: 0,
+        concurrents: 2,
+        hot: false,
+        pageviews: 5466600,
+      },
+      title: "Hold On, We’re Going Home",
+      title_with_featured: "Hold On, We're Going Home (Ft. Majid Jordan)",
+      url: "https://genius.com/Drake-hold-on-were-going-home-lyrics",
+      primary_artist: {
+        api_path: "/artists/130",
+        header_image_url:
+          "https://images.genius.com/19df8d21c9ac35d1503d82f9fdbf367f.1000x563x1.jpg",
+        id: 130,
+        image_url:
+          "https://images.genius.com/99479447145e32877d46d53c7e5bf259.961x961x1.png",
+        is_meme_verified: false,
+        is_verified: false,
+        name: "Drake",
+        url: "https://genius.com/artists/Drake",
+      },
+    },
+  },
+  {
+    highlights: [],
+    index: "song",
+    type: "song",
+    result: {
+      annotation_count: 56,
+      api_path: "/songs/703654",
+      artist_names: "Drake",
+      full_title: "Know Yourself by Drake",
+      header_image_thumbnail_url:
+        "https://images.genius.com/24135e4f75ee1f7b55974dbbac871aad.300x300x1.jpg",
+      header_image_url:
+        "https://images.genius.com/24135e4f75ee1f7b55974dbbac871aad.1000x1000x1.jpg",
+      id: 703654,
+      lyrics_owner_id: 599242,
+      lyrics_state: "complete",
+      path: "/Drake-know-yourself-lyrics",
+      pyongs_count: 1097,
+      song_art_image_thumbnail_url:
+        "https://images.genius.com/24135e4f75ee1f7b55974dbbac871aad.300x300x1.jpg",
+      song_art_image_url:
+        "https://images.genius.com/24135e4f75ee1f7b55974dbbac871aad.1000x1000x1.jpg",
+      stats: {
+        unreviewed_annotations: 0,
+        concurrents: 2,
+        hot: false,
+        pageviews: 5177030,
+      },
+      title: "Know Yourself",
+      title_with_featured: "Know Yourself",
+      url: "https://genius.com/Drake-know-yourself-lyrics",
+      primary_artist: {
+        api_path: "/artists/130",
+        header_image_url:
+          "https://images.genius.com/19df8d21c9ac35d1503d82f9fdbf367f.1000x563x1.jpg",
+        id: 130,
+        image_url:
+          "https://images.genius.com/99479447145e32877d46d53c7e5bf259.961x961x1.png",
+        is_meme_verified: false,
+        is_verified: false,
+        name: "Drake",
+        url: "https://genius.com/artists/Drake",
+      },
+    },
+  },
+  {
+    highlights: [],
+    index: "song",
+    type: "song",
+    result: {
+      annotation_count: 23,
+      api_path: "/songs/2266961",
+      artist_names: "Drake",
+      full_title: "Back to Back by Drake",
+      header_image_thumbnail_url:
+        "https://images.genius.com/0e3dba170124f70a8ed995e7cf3d42eb.300x300x1.png",
+      header_image_url:
+        "https://images.genius.com/0e3dba170124f70a8ed995e7cf3d42eb.1000x1000x1.png",
+      id: 2266961,
+      lyrics_owner_id: 1101903,
+      lyrics_state: "complete",
+      path: "/Drake-back-to-back-lyrics",
+      pyongs_count: 1289,
+      song_art_image_thumbnail_url:
+        "https://images.genius.com/0e3dba170124f70a8ed995e7cf3d42eb.300x300x1.png",
+      song_art_image_url:
+        "https://images.genius.com/0e3dba170124f70a8ed995e7cf3d42eb.1000x1000x1.png",
+      stats: {
+        unreviewed_annotations: 0,
+        hot: false,
+        pageviews: 4965352,
+      },
+      title: "Back to Back",
+      title_with_featured: "Back to Back",
+      url: "https://genius.com/Drake-back-to-back-lyrics",
+      primary_artist: {
+        api_path: "/artists/130",
+        header_image_url:
+          "https://images.genius.com/19df8d21c9ac35d1503d82f9fdbf367f.1000x563x1.jpg",
+        id: 130,
+        image_url:
+          "https://images.genius.com/99479447145e32877d46d53c7e5bf259.961x961x1.png",
+        is_meme_verified: false,
+        is_verified: false,
+        name: "Drake",
+        url: "https://genius.com/artists/Drake",
+      },
+    },
+  },
+  {
+    highlights: [],
+    index: "song",
+    type: "song",
+    result: {
+      annotation_count: 31,
+      api_path: "/songs/196551",
+      artist_names: "Drake (Ft. 2 Chainz & Big Sean)",
+      full_title: "All Me by Drake (Ft. 2 Chainz & Big Sean)",
+      header_image_thumbnail_url:
+        "https://images.genius.com/8767f72ad63d256ebadcd2bf8228c4f5.300x300x1.jpg",
+      header_image_url:
+        "https://images.genius.com/8767f72ad63d256ebadcd2bf8228c4f5.1000x1000x1.jpg",
+      id: 196551,
+      lyrics_owner_id: 113726,
+      lyrics_state: "complete",
+      path: "/Drake-all-me-lyrics",
+      pyongs_count: 510,
+      song_art_image_thumbnail_url:
+        "https://images.genius.com/6557cf1d8660623b66f6a6017d75370c.300x300x1.png",
+      song_art_image_url:
+        "https://images.genius.com/6557cf1d8660623b66f6a6017d75370c.1000x1000x1.png",
+      stats: {
+        unreviewed_annotations: 1,
+        hot: false,
+        pageviews: 4001055,
+      },
+      title: "All Me",
+      title_with_featured: "All Me (Ft. 2 Chainz & Big Sean)",
+      url: "https://genius.com/Drake-all-me-lyrics",
+      primary_artist: {
+        api_path: "/artists/130",
+        header_image_url:
+          "https://images.genius.com/19df8d21c9ac35d1503d82f9fdbf367f.1000x563x1.jpg",
+        id: 130,
+        image_url:
+          "https://images.genius.com/99479447145e32877d46d53c7e5bf259.961x961x1.png",
+        is_meme_verified: false,
+        is_verified: false,
+        name: "Drake",
+        url: "https://genius.com/artists/Drake",
+      },
+    },
+  },
+  {
+    highlights: [],
+    index: "song",
+    type: "song",
+    result: {
+      annotation_count: 25,
+      api_path: "/songs/217270",
+      artist_names: "Drake (Ft. Jhené Aiko)",
+      full_title: "From Time by Drake (Ft. Jhené Aiko)",
+      header_image_thumbnail_url:
+        "https://images.genius.com/6d70f1e6d38e9af4ddb7b3582a40dab8.300x300x1.jpg",
+      header_image_url:
+        "https://images.genius.com/6d70f1e6d38e9af4ddb7b3582a40dab8.1000x1000x1.jpg",
+      id: 217270,
+      lyrics_owner_id: 149650,
+      lyrics_state: "complete",
+      path: "/Drake-from-time-lyrics",
+      pyongs_count: 573,
+      song_art_image_thumbnail_url:
+        "https://images.genius.com/6d70f1e6d38e9af4ddb7b3582a40dab8.300x300x1.jpg",
+      song_art_image_url:
+        "https://images.genius.com/6d70f1e6d38e9af4ddb7b3582a40dab8.1000x1000x1.jpg",
+      stats: {
+        unreviewed_annotations: 0,
+        concurrents: 2,
+        hot: false,
+        pageviews: 3791807,
+      },
+      title: "From Time",
+      title_with_featured: "From Time (Ft. Jhené Aiko)",
+      url: "https://genius.com/Drake-from-time-lyrics",
+      primary_artist: {
+        api_path: "/artists/130",
+        header_image_url:
+          "https://images.genius.com/19df8d21c9ac35d1503d82f9fdbf367f.1000x563x1.jpg",
+        id: 130,
+        image_url:
+          "https://images.genius.com/99479447145e32877d46d53c7e5bf259.961x961x1.png",
+        is_meme_verified: false,
+        is_verified: false,
+        name: "Drake",
+        url: "https://genius.com/artists/Drake",
+      },
+    },
+  },
+  {
+    highlights: [],
+    index: "song",
+    type: "song",
+    result: {
+      annotation_count: 5,
+      api_path: "/songs/2890553",
+      artist_names: "Drake",
+      full_title: "Fake Love by Drake",
+      header_image_thumbnail_url:
+        "https://images.genius.com/ba45a8086a709c65ed46461931e2ea43.300x199x1.jpg",
+      header_image_url:
+        "https://images.genius.com/ba45a8086a709c65ed46461931e2ea43.1000x664x1.jpg",
+      id: 2890553,
+      lyrics_owner_id: 672397,
+      lyrics_state: "complete",
+      path: "/Drake-fake-love-lyrics",
+      pyongs_count: 191,
+      song_art_image_thumbnail_url:
+        "https://images.genius.com/ff88109f9d46d61f0bccf0ebb3b65d96.300x300x1.png",
+      song_art_image_url:
+        "https://images.genius.com/ff88109f9d46d61f0bccf0ebb3b65d96.1000x1000x1.png",
+      stats: {
+        unreviewed_annotations: 0,
+        hot: false,
+        pageviews: 3748146,
+      },
+      title: "Fake Love",
+      title_with_featured: "Fake Love",
+      url: "https://genius.com/Drake-fake-love-lyrics",
+      primary_artist: {
+        api_path: "/artists/130",
+        header_image_url:
+          "https://images.genius.com/19df8d21c9ac35d1503d82f9fdbf367f.1000x563x1.jpg",
+        id: 130,
+        image_url:
+          "https://images.genius.com/99479447145e32877d46d53c7e5bf259.961x961x1.png",
+        is_meme_verified: false,
+        is_verified: false,
+        name: "Drake",
+        url: "https://genius.com/artists/Drake",
+      },
+    },
+  },
+];
